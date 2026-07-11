c34c3756f326291a3d5af6b40443264f4d740c84
lama <lama72316@gmail.com>
2026-07-11 12:33:59 +0300

    Update user.py

---
+++ part2/app/models/user.py
@@ -0,0 +1,24 @@
+import re
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self.first_name = self.validate_name(first_name, "first_name")
+        self.last_name = self.validate_name(last_name, "last_name")
+        self.email = self.validate_email(email)
+        self.is_admin = is_admin
+
+    @staticmethod
+    def validate_name(name, field_name):
+        if not name or len(name) > 50:
+            raise ValueError(f"{field_name} is required and must be <= 50 characters")
+        return name
+
+    @staticmethod
+    def validate_email(email):
+        pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        if not email or not re.match(pattern, email):
+            raise ValueError("Invalid email format")
+        return email
