e3a184e1cb51b714322bbbac824186d0f7577454
alaa2026ali <ali_alaa18@icloud.com>
2026-07-12 10:38:16 +0300

    Register API v1 blueprint in create_app function
    Added API v1 blueprint registration to the app.

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from app.api.v1 import api_v1_blueprint
 
 def create_app():
     app = Flask(__name__)
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
+    # Register API v1 blueprint
+    app.register_blueprint(api_v1_blueprint)
 
     return app
