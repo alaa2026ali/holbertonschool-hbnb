9e5621df840eea3ec2792afe7df6794c68f72ae8
alaa2026ali <ali_alaa18@icloud.com>
2026-07-12 10:23:45 +0300

    Initialize API version 1 with users namespace

---
+++ part2/app/api/v1/__init__.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+from flask_restx import Api
+
+from .users import api as users_ns
+
+api_v1_blueprint = Blueprint(
+    'api_v1',
+    __name__,
+    url_prefix='/api/v1'
+)
+
+api = Api(
+    api_v1_blueprint,
+    title='HBnB API',
+    version='1.0',
+    description='HBnB Application API'
+)
+
+api.add_namespace(users_ns, path='/users')
