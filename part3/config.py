e8a95e87569c214e5a96f2be2d6a7e47df847b71
alaa2026ali <ali_alaa18@icloud.com>
2026-07-27 12:13:05 +0300

    Update config.py

--- part3/config.py
+++ part3/config.py
@@ -3,7 +3,10 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-key')
+    JWT_SECRET_KEY = os.getenv(
+        'JWT_SECRET_KEY',
+        'my-super-secret-jwt-key-for-hbnb-project-2026'
+    )
     DEBUG = False
 
 
