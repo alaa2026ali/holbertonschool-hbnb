adfc56144d3554770cc2191054ebff236f19653c
alaa2026ali <ali_alaa18@icloud.com>
2026-07-27 09:59:57 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from app.api.v1 import api_v1_blueprint
+from config import config
 
-def create_app():
+
+def create_app(config_name='default'):
     app = Flask(__name__)
+
+    # Load configuration
+    app.config.from_object(config[config_name])
 
     # Register API v1 blueprint
     app.register_blueprint(api_v1_blueprint)
