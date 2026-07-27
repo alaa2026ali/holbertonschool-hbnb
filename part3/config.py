3665f53861703f6ebde6c20c3a2535794277ea76
alaa2026ali <ali_alaa18@icloud.com>
2026-07-27 11:27:58 +0300

    Add JWT_SECRET_KEY to configuration
    Added JWT_SECRET_KEY to the Config class.

--- part3/config.py
+++ part3/config.py
@@ -1,11 +1,15 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-key')
     DEBUG = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
 
 config = {
     'development': DevelopmentConfig,
