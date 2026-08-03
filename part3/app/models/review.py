cccee926e71eb1274c2f5f0313b7c38458220a37
lama <lama72316@gmail.com>
2026-08-03 10:02:42 +0300

    Refactor Review class to inherit from PlainBaseModel

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,9 +1,9 @@
-from .base_model import BaseModel
+from .base_model import PlainBaseModel
 from .place import Place
 from .user import User
 
 
-class Review(BaseModel):
+class Review(PlainBaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
         self.text = self.validate_text(text)
