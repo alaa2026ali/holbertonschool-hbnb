9dd9778cff6f20570e6ba07f3177aa9128a6784a
lama <lama72316@gmail.com>
2026-08-03 09:32:29 +0300

    Add SQLAlchemy initialization to create_app

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,22 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1 import api_v1_blueprint
 from config import config
 from flask_jwt_extended import JWTManager
 
-
 jwt = JWTManager()
+db = SQLAlchemy()
 
 
 def create_app(config_name='default'):
     app = Flask(__name__)
-
     # Load configuration
     app.config.from_object(config[config_name])
-
     # Initialize JWT
     jwt.init_app(app)
-
+    # Initialize SQLAlchemy
+    db.init_app(app)
     # Register API v1 blueprint
     app.register_blueprint(api_v1_blueprint)
-
     return app
