4a71c7b23cb2ca8622fc800f16347672459265ef
alaa2026ali <ali_alaa18@icloud.com>
2026-08-24 18:30:23 +0300

    Call seed_data in create_app function
    Added seed_data function call to initialize data.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -25,6 +25,9 @@ def create_app(config_class=config.DevelopmentConfig):
     with app.app_context():
         db.create_all()
 
+        from app.seed import seed_data
+        seed_data()
+
     app.register_blueprint(api_v1_blueprint)
 
     return app
